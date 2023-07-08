117e2c457b6ece95978e2740fd1606ac3a404c53
Tri <trihoang09@gmail.com>
2023-07-07 19:04:00 -0600

    used array methods to complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo" , "Otis", "Garfield"]
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(){
+    const cat = [...cats];
+    cat.push("Broom")
+    return cat;
+}
+function prependCat(){
+    const cat = [...cats];
+    cat.unshift("Arnold");
+    return cat;
+}
+function removeLastCat(){
+    const cat = [...cats];
+    cat.pop();
+    return cat;
+}
+function removeFirstCat(){
+    const cat = [...cats];
+    cat.shift();
+    return cat;
+}
